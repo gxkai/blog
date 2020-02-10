46765d3c2448426de2918af47b9b1470cf0b0cf7
tianyong90 <412039588@qq.com>
2020-02-10 20:42:32 +0800

    script

--- scripts/generate-post-list.ts
+++ scripts/generate-post-list.ts
@@ -6,39 +6,48 @@ import { slugify } from 'transliteration'
 import yargs from 'yargs'
 
 const argv = yargs
-  .command('new', 'create new post', {
-    title: {
-      description: 'post title',
-      demand: true,
-      alias: 't',
+  .command('generate', 'generate post list', {
+    mode: {
+      description: 'mode',
+      default: 'all',
+      demand: false,
+      alias: 'm',
     },
   })
   .help('h').argv
 
-const list = fs.readdirSync(path.resolve(__dirname, '../posts'))
+if (argv[0] === 'generate') {
+  const mode = argv.mode as string
 
-const posts = list.filter(item => item.endsWith('.md')).map(item => item.replace('.md', ''))
+  // 读取 posts 目录下文件及文件夹列表
+  const list = fs.readdirSync(path.resolve(__dirname, '../posts'))
 
-// 读取 md 文章 frontmatter 数组并组合
-const jsonData = posts.map(post => {
-  const { data, content } = matter.read(`./posts/${post}.md`)
+  // 过滤取出 md 文件列表
+  const posts = list.filter(item => item.endsWith('.md')).map(item => item.replace('.md', ''))
 
-  const slugifiedFilename = slugify(data.title, {
-    trim: true,
-    replace: {
-      '——': '-',
-    },
+  // 读取 md 文章 frontmatter 数组并组合
+  const jsonData = posts
+    .map(post => {
+      const { data, content } = matter.read(`./posts/${post}.md`)
+
+      const slugifiedFilename = slugify(data.title, {
+        trim: true,
+        replace: {
+          '——': '-',
+        },
+      })
+
+      return {
+        filename: post,
+        slugifiedFilename,
+        ...data,
+        description: summarize(content).substr(0, 60),
+      }
+    })
+    .filter(item => {})
+
+  // 写入 json
+  fs.writeJson('./posts/posts.json', jsonData, {
+    spaces: 2,
   })
-
-  return {
-    filename: post,
-    slugifiedFilename,
-    ...data,
-    description: summarize(content).substr(0, 60),
-  }
-})
-
-// 写入 json
-fs.writeJson('./posts/posts.json', jsonData, {
-  spaces: 2,
-})
+}
